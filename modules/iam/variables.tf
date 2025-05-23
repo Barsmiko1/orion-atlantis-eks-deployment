9d96ba78981002c4b688aac706d9afe327ffe17d
Barsmiko <86996811+Barsmiko1@users.noreply.github.com>
2025-05-23 18:19:52 +0100

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -3,7 +3,36 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "cluster_oidc_issuer_url" {
-  description = "The URL of the OIDC issuer for the EKS cluster"
+variable "vpc_id" {
+  description = "ID of the VPC"
   type        = string
+}
+
+variable "subnet_ids" {
+  description = "IDs of the subnets"
+  type        = list(string)
+}
+
+variable "min_size" {
+  description = "Minimum size of the node group"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum size of the node group"
+  type        = number
+  default     = 2
+}
+
+variable "desired_size" {
+  description = "Desired size of the node group"
+  type        = number
+  default     = 1
+}
+
+variable "instance_types" {
+  description = "Instance types for the node group"
+  type        = list(string)
+  default     = ["t3.medium"]
 }
