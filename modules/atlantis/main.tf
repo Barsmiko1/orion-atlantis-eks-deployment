62c741863c0ff2eda767fd24d53437c34df8955b
Barsmiko <86996811+Barsmiko1@users.noreply.github.com>
2025-05-23 16:58:00 +0100

    Update main.tf

--- modules/atlantis/main.tf
+++ modules/atlantis/main.tf
@@ -22,9 +22,9 @@ resource "helm_release" "atlantis" {
   repository = "https://runatlantis.github.io/helm-charts"
   chart      = "atlantis"
   namespace  = kubernetes_namespace.atlantis.metadata[0].name
-  timeout    = 900  # 15 minutes timeout for Helm release
+  timeout    = 900  // 15 minutes
   
-  # Using values file for more complex configuration
+  # Use values file for more complex configuration
   values = [
     <<-EOT
     orgAllowlist: "${var.atlantis_repo_allowlist}"
@@ -34,7 +34,7 @@ resource "helm_release" "atlantis" {
       secret: "${var.atlantis_webhook_secret}"
     service:
       type: LoadBalancer
-    # Using the storage configuration based on values.yaml
+    # Use the correct storage configuration based on values.yaml
     volumeClaim:
       enabled: true
       dataStorage: 8Gi
@@ -44,20 +44,54 @@ resource "helm_release" "atlantis" {
       create: true
       annotations:
         eks.amazonaws.com/role-arn: "${var.atlantis_iam_role_arn}"
-    # Set AWS region environment variable
+    # Set environment variables
     environment:
+      ATLANTIS_REPO_ALLOWLIST: "${var.atlantis_repo_allowlist}"
       AWS_REGION: "${var.aws_region}"
+    # Set environment secrets
+    environmentSecrets:
+      - name: ATLANTIS_GH_USER
+        secretKeyRef:
+          name: atlantis-github
+          key: ATLANTIS_GH_USER
+      - name: ATLANTIS_GH_TOKEN
+        secretKeyRef:
+          name: atlantis-github
+          key: ATLANTIS_GH_TOKEN
+      - name: ATLANTIS_GH_WEBHOOK_SECRET
+        secretKeyRef:
+          name: atlantis-github
+          key: ATLANTIS_GH_WEBHOOK_SECRET
+    # Use custom repo config directly in the Helm values
     repoConfig: |
       repos:
-      - id: /.*/
-        workflow: default
-        allowed_overrides: [workflow]
+      - id: github.com/Barsmiko1/orion-atlantis-eks-deployment
+        workflow: custom
+        allowed_overrides: [workflow, apply_requirements]
         allow_custom_workflows: true
+      
+      workflows:
+        custom:
+          plan:
+            steps:
+            - env:
+                name: ATLANTIS_TERRAFORM_VARS
+                command: 'echo "-var atlantis_github_user=$$ATLANTIS_GH_USER -var atlantis_github_token=$$ATLANTIS_GH_TOKEN -var atlantis_repo_allowlist=$$ATLANTIS_REPO_ALLOWLIST -var atlantis_webhook_secret=$$ATLANTIS_GH_WEBHOOK_SECRET"'
+            - init
+            - run: terraform plan $$(eval $$ATLANTIS_TERRAFORM_VARS)
+          apply:
+            steps:
+            - env:
+                name: ATLANTIS_TERRAFORM_VARS
+                command: 'echo "-var atlantis_github_user=$$ATLANTIS_GH_USER -var atlantis_github_token=$$ATLANTIS_GH_TOKEN -var atlantis_repo_allowlist=$$ATLANTIS_REPO_ALLOWLIST -var atlantis_webhook_secret=$$ATLANTIS_GH_WEBHOOK_SECRET"'
+            - init
+            - run: terraform apply $$(eval $$ATLANTIS_TERRAFORM_VARS)
     EOT
   ]
   
   depends_on = [
-    kubernetes_namespace.atlantis
+    kubernetes_namespace.atlantis,
+    kubernetes_secret.atlantis_github
   ]
 }
 
