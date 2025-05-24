98490da637ee75dc75d1d032105ead1e826c229e
michael john <lovejohnmike@gmail.com>
2025-05-24 14:16:20 +0100

    Test Atlantis after fixing AWS CLI issue

--- variables.tf
+++ variables.tf
@@ -64,24 +64,6 @@ variable "instance_types" {
   default     = ["t3.medium"]
 }
 
-variable "atlantis_github_user" {
-  description = "GitHub username for Atlantis"
-  type        = string
-}
 
-variable "atlantis_github_token" {
-  description = "GitHub token for Atlantis"
-  type        = string
-  sensitive   = true
-}
 
-variable "atlantis_repo_allowlist" {
-  description = "List of repositories that Atlantis will work with"
-  type        = string
-}
 
-variable "atlantis_webhook_secret" {
-  description = "Secret for GitHub webhooks"
-  type        = string
-  sensitive   = true
-}
